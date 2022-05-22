36e5f9fad571401694921d6ab5b75b6075f113be
Karlo Sudec <karlo.sudec@brainit.agency>
2022-05-22 19:15:59 +0200

    added new endpoints

--- client-app/src/services/Routes.js
+++ client-app/src/services/Routes.js
@@ -4,10 +4,19 @@ const host = 'https://is2022-boore.herokuapp.com';
 
 export const LOGIN = host + '/login';
 export const REGISTER = host + '/register';
+
 export const CITY_LOV = host + '/city/lov';
 export const GENRE_LOV = host + '/genre/lov';
 
-export const USER_PROFILE = host + '/user';
-export const USER_RECOMMENDATIONS = USER_PROFILE + '/recommendations';
+export const USER_PROFILE = (userId) => host + `/user${userId ? '/' + userId : ''}`;
+export const USER_RECOMMENDATIONS = USER_PROFILE() + '/recommendations';
 export const USER_RECOMMENDATIONS_GENRE = (genreId) => USER_RECOMMENDATIONS + `/?genreId=${genreId}`;
+
+export const WISHLIST = (bookId, wasRead) => host + `/wishlist?bookId=${bookId}${wasRead != null ? '&wasRead=' + wasRead : ''}`;
+
+export const REVIEW = (bookId) => host + `/review${bookId ? '?bookId=' + bookId : ''}`;
+
+export const BOOK = (bookId) => host + `/book/${bookId}`;
+
+export const AUTHOR = (authorId) => host + `/author/${authorId}`;
 
