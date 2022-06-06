a614973d5dcdf1fabc0ca7835be32fccf0596a05
Karlo Sudec <karlo.sudec@brainit.agency>
2022-06-06 02:34:36 +0200

    fix cors maybe

--- src/main/java/hr/unizg/fer/is/boore/boore/AppConfig.java
+++ src/main/java/hr/unizg/fer/is/boore/boore/AppConfig.java
@@ -29,7 +29,7 @@ public class AppConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
+        config.addAllowedOriginPattern("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
